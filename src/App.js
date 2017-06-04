94486d4cbcefcbd7ac60512fb0725c7424360ebd
shoegazerwithak <skrtroot@protonmail.com>
2017-06-04 13:15:37 +0300

    Use VerticalCardList component in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import VerticalCardList from "./components/VerticalCardList";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
+          <VerticalCardList />
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
