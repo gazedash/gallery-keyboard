8b5962783d5311213f8ca5b9989c73488ab55d29
shoegazerwithak <skrtroot@protonmail.com>
2017-06-05 12:10:58 +0300

    Add keyboard controls to VerticalCardList

--- src/components/VerticalCardList.js
+++ src/components/VerticalCardList.js
@@ -37,10 +37,64 @@ import React, { Component } from "react";
 import Card from "./Card";
 import data from "../data.json";
 class VerticalCardList extends Component {
+  state = {
+    currentCard: 0,
+    currentImage: 0,
+    isZoomed: false
+  };
+
+  componentDidMount() {
+    window.addEventListener("keypress", this.handleKeyPress);
+  }
+
+  handleKeyPress = this.handleKeyPress.bind(this);
+  handleKeyPress(e) {
+    const { isZoomed, currentImage, currentCard } = this.state;
+    if (e.code === "KeyG") {
+      this.setState({
+        isZoomed: !isZoomed
+      });
+    }
+    if (e.code === "KeyH" || e.code === "KeyL") {
+      const diff = e.code === "KeyH" ? -1 : e.code === "KeyL" ? 1 : 0;
+      const limit = data[this.state.currentCard].images.length;
+      const currentPlusDiff = currentImage + diff;
+      this.setState(oldState => ({
+        currentImage: this.moveHor(currentPlusDiff, limit)
+      }));
+    }
+    if (e.code === "KeyJ" || e.code === "KeyK") {
+      const diff = e.code === "KeyJ" ? -1 : e.code === "KeyK" ? 1 : 0;
+      const limit = data.length;
+      const currentPlusDiff = currentCard + diff;
+      const newCard = this.moveHor(currentPlusDiff, limit);
+      const newLimit = data[newCard].images.length;
+      this.setState(oldState => ({
+        currentCard: newCard,
+        currentImage: currentImage >= newLimit ? 0 : currentImage
+      }));
+    }
+  }
+
+  moveHor(currentPlusDiff, limit, loop = false) {
+    let res = 0;
+    const lastIndex = limit - 1;
+    if (currentPlusDiff <= 0) {
+      res = loop ? lastIndex : 0;
+    } else {
+      if (currentPlusDiff < limit) {
+        res = currentPlusDiff;
+      } else {
+        res = loop ? 0 : lastIndex;
+      }
+    }
+    return res;
+  }
+
   render() {
     return (
-      <div style={{display: 'flex'}}>
-        {data.map(e => {
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        {data.map((e, i) => {
           const items = e.images.map(image => {
             return {
               image,
@@ -49,9 +103,13 @@ class VerticalCardList extends Component {
             };
           });
           return (
-            <div>
-              <Card key={e.name} items={items} />
-            </div>
+            <Card
+              isZoomed={this.state.isZoomed}
+              active={this.state.currentCard === i}
+              currentImageId={this.state.currentImage}
+              key={e.name}
+              items={items}
+            />
           );
         })}
       </div>
