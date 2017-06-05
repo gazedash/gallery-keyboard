7a50d7e6cda976c8d5c6e6fe0bc75fbb068b15fa
shoegazerwithak <skrtroot@protonmail.com>
2017-06-05 12:09:46 +0300

    Add CardItem active state

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -5,17 +5,26 @@ class CardItem extends Component {
   render() {
     return (
       <div>
+        <div style={this.props.active ? { border: "10px solid red" } : null}>
         <a href={this.props.url}>
-          <img src={this.props.image} />
+          <img height={128} width={128} src={this.props.image} />
         </a>
+        </div>
       </div>
     );
   }
 }
 
 CardItem.propTypes = {
+  isZoomed: PropTypes.bool,
   image: PropTypes.string,
-  url: PropTypes.string
+  url: PropTypes.string,
+  active: PropTypes.bool
+};
+
+CardItem.defaultProps = {
+  isZoomed: false,
+  active: false
 };
 
 export default CardItem;
