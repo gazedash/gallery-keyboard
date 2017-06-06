9a7ad6b1c803fac88bac74e0a4519d6e3b6dad94
shoegazerwithak <skrtroot@protonmail.com>
2017-06-06 15:27:04 +0300

    Refactor CardItem, add style props and default styles, add getters for props

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -2,14 +2,95 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class CardItem extends Component {
+  defaultFixedStyle = {
+    position: "fixed",
+    top: "50%",
+    left: "50%",
+    marginTop: this.marginTop,
+    marginLeft: this.marginLeft
+  };
+
+  get marginTop() {
+    return -(this.props.zoomHeight / 2);
+  }
+
+  get marginLeft() {
+    return -(this.props.zoomWidth / 2);
+  }
+
+  get image() {
+    return this.props.image;
+  }
+
+  get active() {
+    return this.props.active;
+  }
+
+  get url() {
+    return this.props.url;
+  }
+
+  get isZoomed() {
+    return this.props.isZoomed;
+  }
+
+  get fixedStyle() {
+    const { fixedStyle } = this.props;
+    if (Object.keys(fixedStyle).length !== 0) {
+      return fixedStyle;
+    }
+    return this.defaultFixedStyle;
+  }
+
+  get activeStyle() {
+    const { activeStyle, style } = this.props;
+    return this.active ? activeStyle : style;
+  }
+
+  widthOrZoomWidth(zoomed) {
+    const { width, zoomWidth } = this.props;
+    if (zoomed) {
+      return zoomWidth;
+    }
+    return width;
+  }
+
+  heightOrZoomHeight(zoomed) {
+    const { height, zoomHeight } = this.props;
+    if (zoomed) {
+      return zoomHeight;
+    }
+    return height;
+  }
+
+  renderInner(zoomed = false) {
+    if (!this.isZoomed && zoomed) {
+      return null;
+    }
+    const height = this.heightOrZoomHeight(zoomed);
+    const width = this.widthOrZoomWidth(zoomed);
+    return (
+      <div style={this.activeStyle}>
+        <a target="_blank" rel="noopener noreferrer" href={this.url}>
+          <img height={height} width={width} src={this.image} alt="" />
+        </a>
+      </div>
+    );
+  }
+
+  renderFixed() {
+    return (
+      <div style={this.fixedStyle}>
+        {this.renderInner(true)}
+      </div>
+    );
+  }
+
   render() {
     return (
       <div>
-        <div style={this.props.active ? { border: "10px solid red" } : null}>
-        <a href={this.props.url}>
-          <img height={128} width={128} src={this.props.image} />
-        </a>
-        </div>
+        {this.renderInner()}
+        {this.renderFixed()}
       </div>
     );
   }
@@ -19,12 +100,26 @@ CardItem.propTypes = {
   isZoomed: PropTypes.bool,
   image: PropTypes.string,
   url: PropTypes.string,
-  active: PropTypes.bool
+  active: PropTypes.bool,
+  height: PropTypes.number,
+  width: PropTypes.number,
+  zoomHeight: PropTypes.number,
+  zoomWidth: PropTypes.number,
+  style: PropTypes.object,
+  fixedStyle: PropTypes.object,
+  activeStyle: PropTypes.object
 };
 
 CardItem.defaultProps = {
   isZoomed: false,
-  active: false
+  active: false,
+  height: 128,
+  width: 128,
+  zoomHeight: 600,
+  zoomWidth: 600,
+  style: {},
+  fixedStyle: {},
+  activeStyle: {}
 };
 
 export default CardItem;
