e949872615668ca3f06602b985182868a31d9512
shoegazerwithak <skrtroot@protonmail.com>
2017-06-07 10:28:55 +0300

    Add isKey, capitalizeFirstLetter to utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -44,3 +44,11 @@ export function getNext({ isPrevKey, isNextKey, limit, current, loop = false }) 
 export function isObjectEmpty(obj) {
       return Object.keys(obj).length === 0;
 }
+
+export function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export function isKey(key) {
+  return key.endsWith("Key");
+}
