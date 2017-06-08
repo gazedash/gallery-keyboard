119924c463768801a3bfaf486cefc047a512063c
shoegazerwithak <skrtroot@protonmail.com>
2017-06-08 14:39:56 +0300

    App use CardContainerHorizontal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
-import {CardContainerVertical} from "./components/CardContainer";
+import {CardContainerHorizontal} from "./components/CardContainer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <CardContainerVertical/>
+          <CardContainerHorizontal/>
       </div>
     );
   }
