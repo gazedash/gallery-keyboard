099fb2c1d2e0c11f6b70773a064d5cacb0eae083
shoegazerwithak <skrtroot@protonmail.com>
2017-06-08 14:34:36 +0300

    Add onClick to Card, make Card PureComponent

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import CardItem from "../CardItem";
 import PropTypes from "prop-types";
 
-class Card extends Component {
+class Card extends PureComponent {
   get isZoomed() {
     return this.props.isZoomed;
   }
@@ -15,19 +15,29 @@ class Card extends Component {
     return this.props.currentImageId;
   }
 
+  isCurrentActive(index) {
+    return this.currentImageId === index && this.active;
+  }
+
+  isCurrentZoomed(i) {
+    return this.isCurrentActive(i) && this.isZoomed;
+  }
+
   render() {
     return (
       <div style={this.props.style}>
         {this.props.items.map((item, i) => {
-          const active = this.currentImageId === i && this.active;
-          const isCurrentZoomed = this.isZoomed && active;
+          const active = this.isCurrentActive(i);
+          const isCurrentZoomed = this.isCurrentZoomed(i);
           return (
             <CardItem
+              onClick={this.props.onClick}
               isZoomed={isCurrentZoomed}
               active={active}
               key={item.image}
               image={item.image}
               url={item.url}
+              index={i}
               style={this.props.itemStyle}
               activeStyle={this.props.itemActiveStyle}
               fixedStyle={this.props.itemFixedStyle}
@@ -47,16 +57,17 @@ Card.propTypes = {
   style: PropTypes.object,
   itemStyle: PropTypes.object,
   itemFixedStyle: PropTypes.object,
-  itemActiveStyle: PropTypes.object
+  itemActiveStyle: PropTypes.object,
+  onClick: PropTypes.func
 };
 
 Card.defaultProps = {
   items: [],
   isZoomed: false,
   horizontal: true,
-  itemStyle: null,
+  style: null,
   itemFixedStyle: null,
-  itemActiveStyle: null
+  onClick: () => {}
 };
 
 export default Card;
