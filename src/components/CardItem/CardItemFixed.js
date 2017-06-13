77fa47c00bce62408e01ff05cb2057c439f22a22
shoegazerwithak <skrtroot@protonmail.com>
2017-06-13 10:06:08 +0300

    Update CardItemFixed

--- src/components/CardItem/CardItemFixed.js
+++ src/components/CardItem/CardItemFixed.js
@@ -1,26 +1,12 @@
 import React from "react";
 import CardItem from "./CardItem";
-
-const style = {
-  position: "fixed",
-  zIndex: 100,
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  top: "50%",
-  bottom: "50%",
-  margin: "auto",
-  left: 0,
-  right: 0,
-  width: 600,
-  height: 600
-};
-
+import styles from "./CardItemFixed.css";
 export default props => (
   <CardItem
+    className={styles.root}
+    activeClassName={styles.active}
     width={600}
     height={600}
-    style={{ ...props.style, ...style }}
     {...props}
   />
 );
