c107223dd7d5010a8fe6f26100cbbc7e016102e7
shoegazerwithak <skrtroot@protonmail.com>
2017-06-13 10:06:58 +0300

    Update CardContainerVertical

--- src/components/CardContainer/CardContainerVertical.js
+++ src/components/CardContainer/CardContainerVertical.js
@@ -2,12 +2,9 @@ import React from "react";
 import { CardVertical } from "../Card";
 import { CardContainer } from "./index";
 
-const style = { display: "flex" };
-
 export default props => (
   <CardContainer
     itemElement={CardVertical}
-    style={{ ...props.style, ...style }}
     {...props}
   />
 );
