1235b19166ee778f7b3981ab12e9ed02c4060505
shoegazerwithak <skrtroot@protonmail.com>
2017-06-13 09:38:09 +0300

    Update App.test.js to test for passing required props and runtime errors

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe("App", () => {
+  console.error = jest.fn();
+  const div = document.createElement("div");
+  test("renders without crushing", async () => {
+    await ReactDOM.render(<App />, div);
+  });
+  test("doesn't throw errors in runtime", () => {
+    // +test for required props to be passed
+    expect(console.error).not.toBeCalled();
+  });
 });
